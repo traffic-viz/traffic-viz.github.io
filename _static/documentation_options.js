c17e1f5945d480ff9c1a3ef3413124a837e6ef24
xoolive <xoolive@users.noreply.github.com>
2024-01-11 20:13:43 +0000

    deploy: xoolive/traffic@eb6a004739d8089dc28b3bd7666709ef7ff6fa51

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.9.1.dev44',
+    VERSION: '2.9.1.dev45',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
