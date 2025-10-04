906dd62f2f8e18cce72a0619b129ea2abb133b2c
xoolive <xoolive@users.noreply.github.com>
2025-10-04 15:29:52 +0000

    deploy: xoolive/traffic@a35c85068904f8fd57c6225d6e501e293af9e08c

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2.13.post5.dev0+69abde3',
+    VERSION: '2.13.post6.dev0+a35c850',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
